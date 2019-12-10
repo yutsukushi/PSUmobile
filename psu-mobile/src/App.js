ab2d46043cce4b240ca436a3ba063646ce440e07
yutsukushi <ykushibi@gmail.com>
2019-12-09 19:26:24 -0500

    included basic signin page setup

--- psu-mobile/src/App.js
+++ psu-mobile/src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import SignIn from "./pages/SignIn"
+// import CreateAcc from "./pages/CreateAcc";
+// import SearchForm from "./pages/SearchForm";
+// import Saved from "./pages/Saved"
+// import "normalize.css";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+    <span className="">
+      <Switch>
+        <Route exact path="/" component={ SignIn }/>
+        {/* <Route exact path="/createaccount/" component={ CreateAcc }/>
+        <Route exact path="/search" component={ SearchForm } />
+        <Route exact path="/saved" component= { Saved } /> */}
+      </Switch>
+    </span>
+    </Router>
   );
 }
 
--- psu-mobile/src/pages/SignIn.js
+++ psu-mobile/src/pages/SignIn.js
@@ -1,4 +1,4 @@
-import React, { Component } from "React";
+import React, { Component } from "react";
 import "./style.css";
 
 class SignIn extends Component {
@@ -19,7 +19,7 @@ class SignIn extends Component {
         return (
             <div>
                 <div className="psuLogo">
-                    {/* lionpath logo here */}
+                    <img src={require("../images/LionPATH w Icon-PRIMARY Use-2c(287+285)-no acronym.jpg")} alt="lionpath logo"/>
                 </div>
                 <div className="loginForm">
                     {/* login form here */}
@@ -34,8 +34,21 @@ class SignIn extends Component {
                         onChange={this.handleInputChange}
                         />
                     </div>
+                    <div className="inputField">
+                        {/* <span className="username">User ID</span> */}
+                        <input 
+                        className="form-control"
+                        type="text"
+                        placeholder="Password"
+                        name="password"
+                        value={this.state.password}
+                        onChange={this.handleInputChange}
+                        />
+                    </div>
                 </div>
             </div>
         )
     }
 }
+
+export default SignIn;
