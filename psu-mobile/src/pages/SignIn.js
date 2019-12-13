375b5378d31b52cc4f40293a82cd8eb09cb41218
yutsukushi <ykushibi@gmail.com>
2019-12-12 21:14:03 -0500

    piecing together sign in page with CSS, React-BootStrap

--- psu-mobile/src/pages/SignIn.js
+++ psu-mobile/src/pages/SignIn.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
+import { Container, Row, Col, Button } from 'react-bootstrap';
 import "./style.css";
+import "./reset.css";
 
 class SignIn extends Component {
     state = {
@@ -18,42 +20,52 @@ class SignIn extends Component {
     render() {
         return (
             <div>
-                <div className="psuLogo">
-                    <img src={require("../images/LionPATH w Icon-PRIMARY Use-2c(287+285)-no acronym.jpg")} alt="lionpath logo"/>
-                </div>
-                <div className="loginForm">
-                    {/* login form here */}
-                    <form action="/login" method="GET">
-                        <div className="inputField">
-                            {/* <span className="username">User ID</span> */}
-                            <input 
-                            className="form-control"
-                            type="text"
-                            placeholder="User ID"
-                            name="username"
-                            value={this.state.username}
-                            onChange={this.handleInputChange}
-                            />
-                        </div>
-                        <div className="inputField">
-                            {/* <span className="username">User ID</span> */}
-                            <input 
-                            className="form-control"
-                            type="text"
-                            placeholder="Password"
-                            name="password"
-                            value={this.state.password}
-                            onChange={this.handleInputChange}
-                            />
-                        </div>
-                        <div className="buttonContainer">
-                            <button
-                            onClick={this.handleLogInSubmit}>
-                            Sign In
-                            </button>
-                        </div>
-                    </form>
-                </div>
+                <Container>
+                    <Row>
+                        <Col>
+                            <div className="psuLogo">
+                                <img src={require("../images/LionPATH w Icon-PRIMARY Use-2c(287+285)-no acronym.jpg")} alt="lionpath logo"/>
+                            </div>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <div className="loginForm">
+                                {/* login form here */}
+                                <form action="/login" method="GET">
+                                    <div className="inputField">
+                                        {/* <span className="username">User ID</span> */}
+                                        <input 
+                                        className="form-control"
+                                        type="text"
+                                        placeholder="User ID"
+                                        name="username"
+                                        value={this.state.username}
+                                        onChange={this.handleInputChange}
+                                        />
+                                    </div>
+                                    <div className="inputField">
+                                        {/* <span className="username">User ID</span> */}
+                                        <input 
+                                        className="form-control"
+                                        type="text"
+                                        placeholder="Password"
+                                        name="password"
+                                        value={this.state.password}
+                                        onChange={this.handleInputChange}
+                                        />
+                                    </div>
+                                    <div className="buttonContainer">
+                                        <Button
+                                        onClick={this.handleLogInSubmit}>
+                                        Sign in
+                                        </Button>
+                                    </div>
+                                </form>
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
             </div>
         )
     }
